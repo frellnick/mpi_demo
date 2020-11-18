f3e007e67b1e521d392e6c5e002b268f7f5cb4b5
Vincent Brandon <vbrandon@utah.gov>
2020-11-18 10:51:20 -0700

    ENH: update successfully dumping all identity data to MPI identity pool

--- mpi/update.py
+++ mpi/update.py
@@ -12,6 +12,9 @@ import logging
 
 updatelogger = logging.getLogger(__name__)
 
+
+
+
 def _lookup_mpi(ind: int, id_clean: pd.DataFrame, match_dict):
     if ind in match_dict:
         try:
@@ -22,11 +25,14 @@ def _lookup_mpi(ind: int, id_clean: pd.DataFrame, match_dict):
     else:
         return np.nan
 
+
+
 def _multiindex_to_dict(iterable):
     temp = {}
     for pair in iterable:
         temp[pair[0]] = pair[1]
     return temp
+
 
 
 def append_mpi(source_clean, id_clean, matches):
@@ -42,14 +48,17 @@ def append_mpi(source_clean, id_clean, matches):
 
 
 def generate_mpi(unmatched: pd.DataFrame):
-    """ Generate MPI for unmatched identities.  Update Master Person Long table """
+    """ Generate MPI for unmatched identities. """
     updatelogger.info(f'Creating {len(unmatched)} identities')
     temp = unmatched.copy()
     if 'mpi' not in temp.columns:
         temp['mpi'] = None
     temp['mpi'] = temp.mpi.apply(generate_random_mpi)
-    ident_inserts = gen_mpi_insert(temp)
+    return temp
 
+
+
+def write_mpi_data(ident_inserts):
     insert_objects = []
     for iarray in ident_inserts:
         insert_objects.extend([MasterPersonLong(**kwargs) for kwargs in iarray])
@@ -58,13 +67,14 @@ def generate_mpi(unmatched: pd.DataFrame):
         session.bulk_save_objects(insert_objects)
         session.flush()
         session.commit()
-    return temp
+
 
 
 def _remove_index_col(dataframe: pd.DataFrame) -> pd.DataFrame:
     if 'index' in dataframe.columns:
         return dataframe.drop(['index'], axis=1)
     return dataframe
+
 
 
 def expand_match_to_raw(raw, subset, source_clean, id_clean, links_pred):
@@ -76,17 +86,26 @@ def expand_match_to_raw(raw, subset, source_clean, id_clean, links_pred):
         id_clean: deduped identity view, processed during linkage
         links_pred: multi-index from classification step
     """
+    # Append MPI to matched rows in source_clean
     matched, unmatched = append_mpi(source_clean, id_clean, links_pred)
     updatelogger.info(f"Matched {len(matched)} records.  Unmatched {len(unmatched)} records.")
+    
+    # Update Identity Pool
     unmatched = generate_mpi(unmatched)
+    try:
+        write_mpi_data(gen_mpi_insert(matched))
+    except Exception as e:
+        updatelogger.error(f'Could not write matched data to MPI table. {e}')
+    # Error only allowable with matched data.  Unmatched should be empty if data already exists.
+    write_mpi_data(gen_mpi_insert(unmatched))  
+
     updatelogger.debug(f"Unmatched frame showing {len(unmatched[unmatched.mpi.notna()])} \
         of {len(unmatched)} MPI generated.")
     source_clean = union_frames(matched, unmatched)
     updatelogger.info(f"Union source_clean frame shows {len(source_clean[source_clean.mpi.notna()])} mpi.")
 
-    std_columns = [col for col in source_clean if col != 'mpi']
-
     # Expand deduped matches to full identity table
+    std_columns = [col for col in source_clean if col != 'mpi']
     mapped_ids = pd.merge(subset, source_clean, how='left', left_on=std_columns, right_on=std_columns)
 
     # Match mpi with corresponding indices in raw table
